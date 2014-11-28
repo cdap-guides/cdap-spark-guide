0de8c6621899cb8b4230f00757eb9158df0f8902
Artem Makogon <artemmakogon@cask.co>
2014-11-28 20:30:13 +0200

    CDAP-527: changes in docs

--- src/main/java/co/cask/cdap/guides/PageRankApp.java
+++ src/main/java/co/cask/cdap/guides/PageRankApp.java
@@ -28,8 +28,8 @@ public class PageRankApp extends AbstractApplication {
   @Override
   public void configure() {
     setName("PageRankApp");
-    addSpark(new PageRankSpark());
     addStream(new Stream("backlinkURLStream"));
+    addSpark(new PageRankSpark());
     addService("PageRankService", new PageRankHandler());
     try {
       ObjectStores.createObjectStore(getConfigurer(), "pageRanks", Double.class);
